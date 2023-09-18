8c17ee2298ef4607f4cd44961ad1226cd87fce73
lettoretto <137910992+lettoretto@users.noreply.github.com>
2023-09-18 11:55:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+botao.addEventListener('click', function(){
+
+    var nomeCliente = nome.value;
+    var quantiaSorvete = quantia.value;
+
+
+    alert(" " + nomeCliente + "  " + quantiaSorvete + " " + saborSorvete);
+
+    
+});
